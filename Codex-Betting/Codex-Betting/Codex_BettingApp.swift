ce63ec19f2f95a01b31ba6d14fddfffcc7abe324
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-02-04 23:53:57 -0600

    Initial commit

---
+++ Codex-Betting/Codex-Betting/Codex_BettingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Codex_BettingApp.swift
+//  Codex-Betting
+//
+//  Created by Casai User on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Codex_BettingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codex-Betting/Codex-Betting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Codex-Betting
+//
+//  Created by Casai User on 04/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

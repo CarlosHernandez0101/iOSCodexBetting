d072268df0193eae6612504ab5430edbd8ef1f21
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-02-05 23:00:04 -0600

    add Design System (#1)

--- Codex-Betting/Codex-Betting/ContentView.swift
+++ Codex-Betting/Codex-Betting/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Codex")
+            .font(Font.custom(HKGrotesk.light.rawValue, size: 34))
+                        
     }
 }
 
---
+++ Codex-Betting/Codex-Betting/Resources/Design system/Fonts/HKGrotesk.swift
@@ -0,0 +1,14 @@
+//
+//  HKGrotesk.swift
+//  Codex-Betting
+//
+//  Created by Carlos Hernández on 05/02/22.
+//
+
+import Foundation
+
+public enum HKGrotesk: String {
+    case light = "HKGrotesk-Light"
+    case regular = "HKGrotesk-Regular"
+    case bold = "HKGrotesk-Bold"
+}

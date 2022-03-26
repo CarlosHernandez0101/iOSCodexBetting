f23eb5c3dfcedf4a5d5252161213dba98641d8ad
CarlosHernandez0101 <82908248+CarlosHernandez0101@users.noreply.github.com>
2022-03-25 21:48:03 -0600

    Update DataService.swift

--- Codex-Betting/Codex-Betting/DataService/DataService.swift
+++ Codex-Betting/Codex-Betting/DataService/DataService.swift
@@ -69,9 +69,9 @@ final class DataService {
         "El partido elegido está disponible para la promoción",
         "La cuota de la apuesta es mayor a la cuota miníma",
         "Le partido está dentro de las fechas de vigencia",
-        "El monto por apostar está dentro del rango permitido",
         "Los partidos coinciden en fecha y en hora",
         "El resultado a favor y en contra es el mismo en ambas casas de apuestas",
+        "El monto por apostar está dentro del rango permitido",
         "Las ganancias y riesgo coinciden con los de la calculadora",
         "En caso de existir, la apuesta no excede el límite de ganancia"
     ]
